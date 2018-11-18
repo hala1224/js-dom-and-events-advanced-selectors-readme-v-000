9e29bad18ef9f6d04cd3a60714303689ed732c78
Natalie Halawani <hala1224@gmail.com>
2018-11-18 01:42:14 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,10 +1,6 @@
 // declare your functions here...
 function paragraphSelector() {
-  // par = $("p");
-  // for (let i=0; i < par.length; i++) {
-  //   return (par[i]);
-  // }
-  $("p");
+  return $("p");
 }
 function lastImageSelector() {
   $('img:last');
